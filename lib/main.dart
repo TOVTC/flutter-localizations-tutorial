392dcc22d81ed25595f88057ce985e53769cfa6c
TOVTC <veronica.tc.to@outlook.com>
2023-08-03 17:10:38 -0400

    atttempting to make a dropdown list with localized gender options, but running into errors
    still need to figure out onGenerateTitle

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // onGenerateTitle is a callback function that dynamically generates the app's title based on the current BuildContext
-      // The title property is a static property that sets a fixed title for the entire app and does not change dynamically based on the context or state
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
-      
-      // this provides manual localizationDelegates and supportedLocales
-      // localizationsDelegates: const [
-      //   GlobalMaterialLocalizations.delegate,
-      //   GlobalWidgetsLocalizations.delegate,
-      //   GlobalCupertinoLocalizations.delegate,
-      // ],
-      // supportedLocales: const [
-      //   Locale('en'),
-      //   Locale('es'),
-      // ],
+        // onGenerateTitle is a callback function that dynamically generates the app's title based on the current BuildContext
+        // The title property is a static property that sets a fixed title for the entire app and does not change dynamically based on the context or state
+        onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
 
-      // but after improting gen_l10n/app_localizations, you can use this syntax to automatically list localizationDelegates and supportedLocales
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Localizations Sample App')
-    );
+        // this provides manual localizationDelegates and supportedLocales
+        // localizationsDelegates: const [
+        //   GlobalMaterialLocalizations.delegate,
+        //   GlobalWidgetsLocalizations.delegate,
+        //   GlobalCupertinoLocalizations.delegate,
+        // ],
+        // supportedLocales: const [
+        //   Locale('en'),
+        //   Locale('es'),
+        // ],
+
+        // but after improting gen_l10n/app_localizations, you can use this syntax to automatically list localizationDelegates and supportedLocales
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Localizations Sample App'));
   }
 }
 
@@ -60,6 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    String _gender = 'other';
+    List<String> dropdownItems = AppLocalizations.of(context)!.genders.split(":");
+    List<String> dropdownValues = ['male', 'female', 'other'];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -70,12 +73,30 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // example of passed parameter
             Text(AppLocalizations.of(context)!.hello('Cid')),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Text('${AppLocalizations.of(context)!.counterSentence} ${AppLocalizations.of(context)!.nClicks(_counter)}'),
+            // example of pluralization
+            Text(
+                '${AppLocalizations.of(context)!.counterSentence} ${AppLocalizations.of(context)!.nClicks(_counter)}'),
+            DropdownButton(
+              value: _gender,
+              onChanged: (gender) {
+                setState(() {
+                  print(gender);
+                  // _gender = gender!;
+                });
+              },
+              items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
           ],
         ),
       ),
