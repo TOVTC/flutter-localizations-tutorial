0e73cd5a08617c008234db482b76a9aba5b81f9b
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-08-04 13:32:38 -0400

    added example of selecting options

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _gender = 'other';
 
   void _incrementCounter() {
     setState(() {
@@ -59,34 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    String? _gender;
-    // List<String> dropdownItems = AppLocalizations.of(context)!.genders.split(":");
-    // List<DropdownMenuItem<String>> items = [];
-    // Map<String, String> genderList = {
-    //   'male': dropdownItems[0],
-    //   'female': dropdownItems[1],
-    //   'other': dropdownItems[2]
-    // };
-    
-    // void listItems () {
-    //   setState(() {
-    //   for (var item in genderList.entries) {
-    //     items.add(DropdownMenuItem<String>(
-    //       value: item.key,
-    //       child: Text(item.value),
-    //     ));
-    //   }
-    //   });
-    // }
-
-    // listItems();
-
-    void changeGender(gender) {
-                setState(() {
-                  _gender = gender!;
-                  print(_gender);
-                });
-    }
+    List<String> dropdownItems = ['male', 'female', 'other'];
 
     return Scaffold(
       appBar: AppBar(
@@ -107,61 +81,23 @@ class _MyHomePageState extends State<MyHomePage> {
             // example of pluralization
             Text(
                 '${AppLocalizations.of(context)!.counterSentence} ${AppLocalizations.of(context)!.nClicks(_counter)}'),
+            // example of providing options
             DropdownButton(
               value: _gender,
               onChanged: (gender) {
-                changeGender(gender);
+                setState(() {
+                  _gender = gender!;
+                });
               },
-              // items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
-              //   return DropdownMenuItem<String>(
-              //     value: value,
-              //     child: Text(value),
-              //   );
-              // }).toList(),
-              // items: [
-              //   DropdownMenuItem<String>(
-              //     value: 'male',
-              //     child: Text(dropdownItems[0]),
-              //   ),
-              //   DropdownMenuItem<String>(
-              //     value: 'female',
-              //     child: Text(dropdownItems[1]),
-              //   ),
-              //   DropdownMenuItem<String>(
-              //     value: 'other',
-              //     child: Text(dropdownItems[2]),
-              //   ),
-              // ],
-              // items: items
-              // items: [
-              //   DropdownMenuItem<String>(
-              //     value: 'male',
-              //     child: Text(AppLocalizations.of(context)!.genders('male')),
-              //   ),
-              //   DropdownMenuItem<String>(
-              //     value: 'female',
-              //     child: Text(AppLocalizations.of(context)!.genders('female')),
-              //   ),
-              //   DropdownMenuItem<String>(
-              //     value: 'other',
-              //     child: Text(AppLocalizations.of(context)!.genders('other')),
-              //   ),
-              // ],
-              items: [
-                DropdownMenuItem<String>(
-                  value: 'male',
-                  child: Text('male'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'female',
-                  child: Text('female'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'other',
-                  child: Text('other'),
-                ),
-              ],
+              items:
+                  dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(AppLocalizations.of(context)!.genders(value)),
+                );
+              }).toList(),
             ),
+            Text(AppLocalizations.of(context)!.pronoun(_gender)),
           ],
         ),
       ),
